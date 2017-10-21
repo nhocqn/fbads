ce103bc36ce0942286b28a41c25c7a2e081e1354
Onwuasoanya George <ndu4george@gmail.com>
2017-10-21 15:04:16 +0100

    first commit

---
+++ app/my_helper.php
@@ -0,0 +1,178 @@
+<?php
+
+function app_constants()
+{
+    return [
+        'TOKEN_EXPIRED' => 'You session has expired, please login to renew your session',
+
+
+//        Member
+        'INVALID_MEMBER' => "The member does not exist",
+        'PASSWORD_CHANGE_SUCCESSFUL' => "Password change successful.",
+        'INVALID_PASSWORD' => 'The password you specified does not match your registered password, please try again with the right password.',
+        'INVALID_OLD_PASSWORD' => 'The old password your specified does not match your registered password, please try again with the right password.',
+        'INVALID_EMAIL' => "There is no account linked to the email you provided.",
+        'MEM_PROFILE_UPDATED' => "Your profile has been updated successful",
+
+//        Forgot Password
+        'PASSWORD_RESET_LINK_SENT' => "Reset link has been sent to the email.",
+
+
+//        AUTH
+
+        'TOKEN_INVALIDATED' => 'Your session has expired, please log in to continue',
+        'TOKEN_INVALID' => 'Illegal access. Please log in or register',
+        'TOKEN_GEN' => 'Successfully logged you into the app',
+        'INVALID_CREDENTIALS' => 'The login credentials you used is not valid, please check and retry.',
+        'TOKEN_REFRESHED' => 'Your session has been refreshed.',
+        'MEMBER' => 'Member details',
+        'MEMBER_NOT_FOUND' => 'Member details does not exist',
+        'TOKEN_CREATION_ERR' => 'We are sorry but we could not log you into the app. A group of experts are already on this matter. ',
+
+
+//        MISCELLANEOUS
+        'PROCESSING' => 'The action is being processed and would be resolved soon.',
+
+        'RESET_EMAIL_SENT' => 'The email reset link has been successfully sent to your email. Use the link provided in the email to reset your password. ',
+        'EMAIL_FROM' => "Event Hub",
+
+//        CODE
+        'VALIDATION_EXCEPTION_CODE' => '402',
+        'TOKEN_INVALID_CODE' => '401',
+        'TOKEN_INVALIDATED_CODE' => '406',
+        'EXCEPTION_CODE' => '500',
+
+//       Exceptions
+        'VALIDATION_EXCEPTION' => 'You did not fill one or more required fields in the form. Please fill all the required fields.',
+        'INVALID_EMAIL_EXCEPTION' => 'We could not send a reset link to the email because the email does not have an account attached to it.',
+        'REG_VALIDATION_EXCEPTION' => 'The email you chose already has an account. Try logging into the app with your email  and retry your registration again.',
+        'EXCEPTION' => 'This is embarrassing. Something went wrong while trying to process your request but a group of experts are already on this matter.',
+    ];
+}
+
+
+function generateRandomString($length = 6)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+
+    return $randomString;
+}
+
+
+function isValidEmail($email)
+{
+    return (boolean)filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email);
+}
+
+
+function sendEmail($message_data, $subject, $from, $to, $type = null)
+{
+    if (!$type) {
+        $type = "default";
+    }
+    $info['message'] = $message_data;
+    $info['from'] = $from;
+    $info['email'] = $to;
+    $info['subject'] = $subject;
+
+
+    \Illuminate\Support\Facades\Mail::send('emails.' . $type, compact('message_data', 'info'), function ($message) use ($info) {
+        $message->from(getAppEmail(), getAppName(   ));
+        $message->bcc($info['email'])->subject($info['subject']);
+    });
+
+}
+
+function getAppName(){
+    return "Ad Manager";
+}
+
+function getAppEmail(){
+    return "ndu4george@gmail.com";
+}
+
+function search_query_constructor($searchString, $col)
+{
+    $dataArray = (array_filter(explode(" ", trim($searchString))));
+    $constructor_sql = "(";
+    if (count($dataArray) < 1) {
+        return " 1 ";
+    }
+    if (is_array($col)) {
+        foreach ($col as $col_name) {
+            if ($col_name !== $col[0]) {
+                $constructor_sql .= " OR ";
+            }
+            for ($i = 0; $i < count($dataArray); $i++) {
+                if (count($dataArray) - 1 === $i) {
+                    $constructor_sql .= "$col_name LIKE '%{$dataArray[$i]}%' ";
+                } else {
+                    $constructor_sql .= "$col_name LIKE '%{$dataArray[$i]}%' OR ";
+                }
+            }
+        }
+    } else {
+        for ($i = 0; $i < count($dataArray); $i++) {
+            if (count($dataArray) - 1 === $i) {
+                $constructor_sql .= "$col LIKE '%{$dataArray[$i]}%' ";
+            } else {
+                $constructor_sql .= "$col LIKE '%{$dataArray[$i]}%' OR ";
+            }
+        }
+    }
+    $constructor_sql .= ")";
+    return $constructor_sql;
+}
+
+function multi_unset($array, $keys)
+{
+    if (is_array($array)) {
+        foreach ($keys as $key) {
+            unset($array[$key]);
+        }
+
+        return $array;
+
+    } else {
+        return null;
+    }
+}
+
+
+
+function encrypt_decrypt($action, $string)
+{
+    try {
+
+        $output = false;
+
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'H899JHShjdfhjhejkse@14447DP';
+        $secret_iv = 'TYEHVn0dUIK888JSBGDD';
+
+        // hash
+        $key = hash('sha256', $secret_key);
+
+        // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+        if ($action == 'encrypt') {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        } else if ($action == 'decrypt') {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+
+        return $output;
+
+    } catch (Exception $e) {
+        return false;
+    }
+}
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ getAppName() }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '184665608748153',
+                xfbml      : true,
+                version    : 'v2.10'
+            });
+            FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                   {{ getAppName() }}
+                </div>
+
+                <div class="links">
+                    <div
+                            class="fb-like"
+                            data-share="true"
+                            data-width="450"
+                            data-show-faces="true">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
